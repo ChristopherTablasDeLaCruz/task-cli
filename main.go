f0568273671e94d3bbb0ae6f3d6b88b752dbbe0d
ChristopherTablasDeLaCruz <101226939+ChristopherTablasDeLaCruz@users.noreply.github.com>
2026-01-07 18:00:34 -0800

    Initial commit: defined Tast struct and main

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func main() {
+	fmt.Println("Task Management CLI")
+}
